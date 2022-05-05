2051c72596cf618bbe56cc854f69a8f12070b01a
Marcelo Lazaroni <lazamar@fb.com>
2022-05-05 11:31:58 +0100

    Use 'path' module to join env paths

--- server/env.js
+++ server/env.js
@@ -1,9 +1,12 @@
 // This file contains environment variables that are
 // useful for functions throughout the server.
+const path = require("path");
+
+const PROJECT_ROOT = path.join(__dirname, "..");
 
 const DB_NAME = "library.db";
 
-module.exports = {
+const env = {
   // This is the path to the `/server` directory in the local machine
   // that is running this code. We will use it to especify full paths
   // when importing local modules.
@@ -11,5 +14,10 @@ module.exports = {
 
   // Path to the database file. It lives at the project's root, which
   // is one directory above the directory this file is in.
-  DB_PATH: __dirname + "/../" + DB_NAME,
+  DB_PATH: path.join(PROJECT_ROOT, DB_NAME),
+
+  // Path to the compiled client code.
+  CLIENT_PATH: path.join(PROJECT_ROOT, "client/index.html"),
 }
+
+module.exports = env;
